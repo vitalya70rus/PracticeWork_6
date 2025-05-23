2a025e77abd123bcdec679504dd67a4dac5a980e
vitalya70rus <vitalya70rus@gmail.com>
2025-05-23 11:38:20 +0700

    task 3

--- 3.py
+++ 3.py
@@ -1,21 +1,26 @@
-import datetime
+from datetime import date
 
 class Task:
-    def __init__(self, DateStart, DateFinish, Description):
-        self.DateStart = DateStart
-        self.DateFinish = DateFinish
-        self.Description = Description
+    def __init__(self, date_start, date_finish, description):
+        self.date_start = date_start
+        self.date_finish = date_finish
+        self.description = description
 
-tasks = [Task(datetime.date(2020,1,1), datetime.date(2020,1,13), 'new year'),
-Task(datetime.date(2020, 2, 23), datetime.date(2020, 3, 8), '23 feb and 8 mar'),
-Task(datetime.date(2020, 4, 20),  datetime.date(2020, 5, 20), 'spring'),
-Task(datetime.date(2020, 6, 1), datetime.date(2020, 8, 31), 'summer'),
-Task(datetime.date(2020, 9, 1), datetime.date(2020, 12, 31), 'autumn and winter')]
+tasks = \
+[
+    Task(date(2020, 1, 1), date(2020, 1, 13), 'new year'),
+    Task(date(2020, 2, 23), date(2020, 3, 8), '23 feb and 8 mar'),
+    Task(date(2020, 4, 20), date(2020, 5, 20), 'spring'),
+    Task(date(2020, 6, 1), date(2020, 8, 31), 'summer'),
+    Task(date(2020, 9, 1), date(2020, 12, 31), 'autumn and winter')
+]
 
-max_datefinish = datetime.date(2020, 1, 1)
+max_finish = date(2020, 1, 1)
+
 for task in tasks:
-    if task.DateFinish > max_datefinish:
-        max_datefinish = task.DateFinish
+    if task.date_finish > max_finish:
+        max_finish = task.date_finish
+
 for task in tasks:
-    if max_datefinish == task.DateFinish:
-        print(task.DateStart, task.DateFinish, task.Description)
+    if task.date_finish == max_finish:
+        print(task.date_start, task.date_finish, task.description)
